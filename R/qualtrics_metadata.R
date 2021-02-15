dc87a1c7002876c787d9d17e32cc35a0f486aa97
Jeffrey Stevens <jeffrey.r.stevens@gmail.com>
2021-02-15 14:06:46 -0600

    Create example data file qualtrics_metadata

---
+++ R/qualtrics_metadata.R
@@ -0,0 +1,26 @@
+#' Metadata from simulated Qualtrics study
+#'
+#' A dataset containing the metadata from a standard Qualtrics survey with
+#' browser metadata collected. These data were randomly generated using
+#' [ip_tools::rand_ip] and [rgeolocate::ip2location] functions.
+#'
+#' @format A data frame with 100 rows and 16 variables:
+#' \describe{
+#'   \item{StartDate}{date and time data collection started, in ISO 8601 format}
+#'   \item{EndDate}{date and time data collection ended, in ISO 8601 format}
+#'   \item{Status}{flag for preview (Survey Preview) vs. implemented survey (IP Address) entries}
+#'   \item{IPAddress}{participant IP address (truncated for anonymity)}
+#'   \item{Progress}{percentage of survey completed}
+#'   \item{Duration (in seconds)}{duration of time required to complete survey, in seconds}
+#'   \item{Finished}{flag for whether survey was completed (TRUE) or progress was < 100 (FALSE)}
+#'   \item{RecordedDate}{date and time survey was recorded, in ISO 8601 format}
+#'   \item{ResponseId}{random ID for participants}
+#'   \item{LocationLatitude}{latitude geolocated from IP address}
+#'   \item{LocationLongitude}{longitude geolocated from IP address}
+#'   \item{UserLanguage}{language set in Qualtrics}
+#'   \item{Browser}{user web browser type}
+#'   \item{Version}{user web browser version}
+#'   \item{Operation System}{user operating system}
+#'   \item{Resolution}{user screen resolution}
+#' }
+"qualtrics_metadata"
