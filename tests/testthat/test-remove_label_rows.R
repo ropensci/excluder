1f37433a1cea6cb723aebb811ae7152bc3f49a2d
Jeffrey Stevens <jeffrey.r.stevens@gmail.com>
2021-04-24 10:34:51 -0500

    Create tests for remove_label_rows()

---
+++ tests/testthat/test-remove_label_rows.R
@@ -0,0 +1,23 @@
+test_that("non-Qualtrics data is identified", {
+  expect_message(remove_label_rows(iris), "This data frame does not appear to be a Qualtrics data set.")
+})
+
+test_that("columns are converted when requested", {
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$StartDate, "POSIXct"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$EndDate, "POSIXct"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$RecordedDate, "POSIXct"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$Progress, "numeric"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$`Duration (in seconds)`, "numeric"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$LocationLatitude, "numeric"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = TRUE)$LocationLongitude, "numeric"))
+})
+
+test_that("columns are not converted when not requested", {
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$StartDate, "character"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$EndDate, "character"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$RecordedDate, "character"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$Progress, "character"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$`Duration (in seconds)`, "character"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$LocationLatitude, "character"))
+  expect_true(inherits(remove_label_rows(qualtrics_raw, convert = FALSE)$LocationLongitude, "character"))
+})
