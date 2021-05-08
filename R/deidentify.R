80c7b98f20e393bea8588e49968ff5a8559e458c
Jeffrey Stevens <jeffrey.r.stevens@gmail.com>
2021-05-08 10:11:12 -0500

    Create deidentify() function

---
+++ R/deidentify.R
@@ -0,0 +1,53 @@
+#' Remove columns that could include identifiable information
+#'
+#' @description
+#' The `deidentify()` function selects out columns from
+#' [Qualtrics](https://qualtrics.com) surveys that may include identifiable
+#' information such as IP address, location, or computer characteristics.
+#'
+#' @details
+#' The function offers two levels of deidentification. The default strict level
+#' removes columns associated with IP address and location and computer
+#' information (browser type and version, operating system, and screen
+#' resolution). The non-strict level removes only columns associated with
+#' IP address and location.
+#'
+#' Typically, deidenification should be used at the end of a processing pipeline
+#' so that these columns can be used to exclude rows.
+#'
+#' @param .data Data frame (downloaded from Qualtrics).
+#' @param strict Logical indicating whether to use strict or non-strict level
+#' of deidentification. Strict removes computer information columns in addition
+#' to IP address and location.
+#'
+#' @return
+#' An object of the same type as `.data` that excludes Qualtrics columns with
+#' identifiable information.
+#' @export
+#'
+#' @examples
+#' names(qualtrics_numeric)
+#'
+#' # Remove IP address, location, and computer information columns
+#' deid <- deidentify(qualtrics_numeric)
+#' names(deid)
+#'
+#' # Remove only IP address and location columns
+#' deid2 <- deidentify(qualtrics_numeric, strict = FALSE)
+#' names(deid2)
+deidentify <- function(.data, strict = TRUE) {
+
+  # Define columns to remove
+  location_cols <- c("IPAddress", "LocationLatitude", "LocationLongitude", "UserLanguage", "IP Address", "Location Latitude", "Location Longitude", "User Language")
+  computer_cols <- c("Browser", "Version", "Operating System", "Resolution")
+
+  # Remove columns
+  .data <- .data %>%
+    dplyr::select(!dplyr::any_of(location_cols))
+
+  if (strict == TRUE) {
+    .data <- .data %>%
+      dplyr::select(!dplyr::contains(computer_cols))
+  }
+  invisible(.data)
+}
