bb795b89a4de764ea1f4a3a3ab75b48ec7796945
Jeffrey R. Stevens <jeffrey.r.stevens@gmail.com>
2022-04-08 15:13:18 -0500

    Create tests for qualtrics_fetch data

--- tests/testthat/test-qualtrics_data.R
+++ tests/testthat/test-qualtrics_data.R
@@ -1,8 +1,13 @@
 # Test qualtrics_raw()
 
+test_that("Column names are correct", {
+  expect_true(names(qualtrics_raw)[1] == "StartDate")
+  expect_true(names(qualtrics_raw)[3] == "Status")
+})
+
 test_that("Columns are of correct type in character data", {
-  expect_type(qualtrics_text$Status, "character")
-  expect_type(qualtrics_text$Finished, "logical")
+  expect_type(qualtrics_raw$Status, "character")
+  expect_type(qualtrics_raw$Finished, "character")
 })
 
 test_that("Data sets include exclusion criteria", {
@@ -41,6 +46,11 @@ test_that("Data sets include exclusion criteria", {
 
 # Test qualtrics_numeric()
 
+test_that("Column names are correct", {
+  expect_true(names(qualtrics_numeric)[1] == "StartDate")
+  expect_true(names(qualtrics_numeric)[3] == "Status")
+})
+
 test_that("Columns are of correct type in numeric data", {
   expect_type(qualtrics_numeric$Status, "double")
   expect_type(qualtrics_numeric$Finished, "double")
@@ -78,6 +88,11 @@ test_that("Data sets include exclusion criteria", {
 
 # Test qualtrics_text()
 
+test_that("Column names are correct", {
+  expect_true(names(qualtrics_text)[1] == "StartDate")
+  expect_true(names(qualtrics_text)[3] == "Status")
+})
+
 test_that("Columns are of correct type in character data", {
   expect_type(qualtrics_text$Status, "character")
   expect_type(qualtrics_text$Finished, "logical")
@@ -112,3 +127,46 @@ test_that("Data sets include exclusion criteria", {
     )) > 0
   )
 })
+
+# Test qualtrics_fetch()
+
+test_that("Column names are correct", {
+  expect_true(names(qualtrics_fetch)[1] == "Start Date")
+  expect_true(names(qualtrics_fetch)[3] == "Response Type")
+})
+
+test_that("Columns are of correct type in fetch data", {
+  expect_type(qualtrics_fetch$`Response Type`, "double")
+  expect_type(qualtrics_fetch$Finished, "double")
+})
+
+test_that("Data sets include exclusion criteria", {
+  # Check for preview surveys
+  expect_true(any(qualtrics_fetch$`Response Type` == 1))
+
+  # Check for incomplete progress
+  expect_true(any(qualtrics_fetch$Finished == 0))
+
+  # Check for durations < 100 s
+  expect_true(any(qualtrics_fetch$`Duration (in seconds)` < 100))
+
+  # Check for small screen resolution
+  expect_true(any(as.numeric(stringr::str_split(qualtrics_fetch$`Click to write the question text - Resolution`,
+                                                "x",
+                                                simplify = TRUE
+  )[, 1]) < 1000))
+
+  # Check for duplicate IP addresses
+  expect_true(nrow(janitor::get_dupes(qualtrics_fetch, `IP Address`)) > 0)
+
+  # Check for duplicate latitude and longitude
+  expect_true(
+    nrow(janitor::get_dupes(
+      qualtrics_fetch,
+      dplyr::any_of(
+        c("Location Latitude", "Location Longitude")
+      )
+    )) > 0
+  )
+})
+
